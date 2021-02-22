e1948b2c7de6d56df1df04a6eef8ad805aaaff3e
JorgeFuentesCasillas <j.fuentes.casillas@gmail.com>
2021-02-22 16:15:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Tarea004_SpotifyApp
---
+++ app/src/main/java/com/example/tarea004_spotifyapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tarea004_spotifyapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
